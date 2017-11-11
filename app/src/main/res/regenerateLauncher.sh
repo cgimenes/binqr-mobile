09af547379522b8204b681cd36de496d6dc2bcd1
cgimenes <marcelo.gimenes.oliveira@gmail.com>
2017-11-11 03:20:02 -0200

    Changing app launcher

---
+++ app/src/main/res/regenerateLauncher.sh
@@ -0,0 +1,6 @@
+convert launcher512px.png -resize 72x72 mipmap-hdpi/ic_launcher.png
+convert launcher512px.png -resize 48x48 mipmap-mdpi/ic_launcher.png
+convert launcher512px.png -resize 96x96 mipmap-xhdpi/ic_launcher.png
+convert launcher512px.png -resize 144x144 mipmap-xxhdpi/ic_launcher.png
+convert launcher512px.png -resize 192x192 mipmap-xxxhdpi/ic_launcher.png
+
